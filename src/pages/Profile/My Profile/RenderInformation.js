0e03e01bae96487b78fb6ceeb4a4e0e7e8599a4f
Roshan Singh <roshan2152@gmail.com>
2023-07-06 10:39:17 +0530

    Settings page & payments page UI implemented

--- src/pages/Profile/My Profile/RenderInformation.js
+++ src/pages/Profile/My Profile/RenderInformation.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { Grid, TextField } from "@mui/material";
+import useStyles from "./styles";
 
-const RenderInformation = ({ field, label, value, isEditing, handleChange }) => (
-  <Grid item xs={6} key={field} style={{ marginBottom: "30px" }}>
-    <TextField
-      label={label}
-      variant="outlined"
-      size="small"
-      value={value}
-      onChange={(e) => handleChange(field, e.target.value)}
-      disabled={!isEditing}
-    />
-  </Grid>
-);
-
-export default RenderInformation;
+export default function RenderInformation({
+  field,
+  label,
+  value,
+  isEditing,
+  handleChange,
+}) {
+  const classes = useStyles();
+  return (
+    <Grid item xs={6} key={field} style={{ marginBottom: "30px" }}>
+      <TextField
+        label={label}
+        variant="outlined"
+        size="small"
+        value={value}
+        onChange={(e) => handleChange(field, e.target.value)}
+        disabled={!isEditing}
+        focused
+        className={classes.textField}
+      />
+    </Grid>
+  );
+}
