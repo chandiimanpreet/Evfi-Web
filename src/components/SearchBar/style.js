9653412d6650605169a9335b508d8fdd25d27f36
Gautamkumarsolanki <42197609+Gautamkumarsolanki@users.noreply.github.com>
2024-01-28 00:07:59 +0530

    minor changes

--- src/components/SearchBar/style.js
+++ src/components/SearchBar/style.js
@@ -247,15 +247,11 @@ export const useStyles = makeStyles({
       "&  .MuiOutlinedInput-root": {
         height: "3rem!important",
       },
-      "& .MuiOutlinedInput-root": {
-        "& fieldset": {
-          borderRadius: "10px",
-        },
-      },
-
+      
       "& .MuiOutlinedInput-root": {
         "& fieldset": {
           //right:"1rem",
+          borderRadius: "10px",
           width: "91%",
           fontSize: "0.4rem",
           top: "0.1rem",
