7c18bdfdc324566bf1955185db1950639351bd31
Somnath Das <somnathdas@Somnaths-MacBook-Air.local>
2024-01-28 06:16:33 +0530

    make navigationbar small for mobile

--- src/components/SearchBar/PlaceSearchingNavigationbar.js
+++ src/components/SearchBar/PlaceSearchingNavigationbar.js
@@ -1,54 +1,101 @@
 import { AppBar, Toolbar, TextField, Autocomplete, Box } from "@mui/material";
-import { Bolt as BoltIcon, Directions as DirectionsIcon } from "@mui/icons-material";
+import {
+  Bolt as BoltIcon,
+  Directions as DirectionsIcon,
+} from "@mui/icons-material";
 import { useStyles } from "./style";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const PlaceSearchingNavigationbar = ({
-    handleOnclickSource, distanceData, onChangeRoute, searchLocationCoordinates,
-    setSearchLocationCoordinates }) => {
+  handleOnclickSource,
+  distanceData,
+  onChangeRoute,
+  searchLocationCoordinates,
+  setSearchLocationCoordinates,
+}) => {
+  // Styles
+  const classes = useStyles();
 
-    // Styles
-    const classes = useStyles();
+  return (
+    <motion.div>
+      <AppBar
+        className={classes.searchNavigation}
+        sx={{
+          width: { xs: "100%", sm: "100%", md: "48%" },
+          left: { xs: "0", sm: "0", md: "calc(50% - 23vw)!important" },
+        }}
+      >
+        <Toolbar
+          className={classes.toolbarstyle}
+          sx={{
+            height: { xs: "1rem", sm: "auto", md: "auto%" },
+          }}
+        >
+          <BoltIcon
+            className={classes.searchboltIcon}
+            sx={{
+              fontSize: {
+                xs: "1.5rem!important",
+                sm: "2.3rem!important",
+                md: "2.5rem!important",
+              },
+            }}
+          />
+          <Box className={classes.extendinputroot}>
+            <Autocomplete
+              clearIcon
+              disablePortal
+              id="combo-box-demo"
+              value={searchLocationCoordinates.searchlocation.label}
+              options={distanceData}
+              data-shrink="false!important"
+              className={classes.searchPlaceAutoComplete}
+              sx={{
+                padding: {
+                  xs: "0.1rem  0.1rem 0.5rem 0.1rem",
+                  sm: "0.5rem 0.5rem",
+                },
+              }}
+              renderInput={(params) => (
+                <TextField
+                  value={searchLocationCoordinates.searchlocation.label}
+                  onChange={onChangeRoute}
+                  placeholder="Search any location"
+                  inputProps={{ sx: { color: "#fff" }, maxLength: 12 }}
+                  className={classes.popDesign}
+                  {...params}
+                  id="destination-textfield"
+                />
+              )}
+              onChange={(event, newValue) => {
+                console.log(newValue);
+                if (newValue) {
+                  setSearchLocationCoordinates({
+                    ...searchLocationCoordinates,
+                    searchlocation: {
+                      coordinates: newValue.coordinates,
+                      label: newValue.label,
+                    },
+                  });
+                } else {
+                  setSearchLocationCoordinates({
+                    searchlocation: { coordinates: null, label: "" },
+                  });
+                }
+              }}
+            />
+          </Box>
+          <DirectionsIcon
+            onClick={handleOnclickSource}
+            className={classes.directionsIcon}
+            sx={{
+              fontSize: { xs: "1.5rem", sm: "2.3rem", md: "2.5rem!important" },
+            }}
+          />
+        </Toolbar>
+      </AppBar>
+    </motion.div>
+  );
+};
 
-    return (
-        <motion.div>
-            <AppBar className={classes.searchNavigation} sx={{ width: {xs: '100%', sm: '100%', md:'48%'},left: {xs: '0', sm: '0',md:"calc(50% - 23vw)!important"}}} >
-                <Toolbar className={classes.toolbarstyle} >
-                    <BoltIcon className={classes.searchboltIcon}  sx={{ fontSize: {xs: '2rem!important', sm: '2.3rem!important',md:'2.5rem!important'}}} />
-                    <Box className={classes.extendinputroot}>
-                        <Autocomplete clearIcon disablePortal id="combo-box-demo"
-                            value={searchLocationCoordinates.searchlocation.label}
-                            options={distanceData} data-shrink="false!important"
-                            className={classes.searchPlaceAutoComplete}
-                            sx={{ padding: {xs: '0.1rem  0.1rem 0.5rem 0.1rem', sm: '0.5rem 0.5rem'}}}
-                            renderInput={(params) =>
-                                <TextField
-                                    value={searchLocationCoordinates.searchlocation.label}
-                                    onChange={onChangeRoute} placeholder="Search any location"
-                                    inputProps={{ sx: { color: '#fff' }, maxLength: 12 }}
-                                    className={classes.popDesign} {...params}
-                                    id="destination-textfield"
-                                />
-                            }
-                            onChange={(event, newValue) => {
-                                console.log(newValue);
-                                if (newValue) {
-                                    setSearchLocationCoordinates({ ...searchLocationCoordinates, searchlocation: { coordinates: newValue.coordinates, label: newValue.label } })
-                                } else {
-                                    setSearchLocationCoordinates({ searchlocation: { coordinates: null, label: '' } })
-                                }
-                            }}
-                        />
-                    </Box>
-                    <DirectionsIcon
-                        onClick={handleOnclickSource}
-                        className={classes.directionsIcon}
-                        sx={{ fontSize: {xs: '2rem', sm: '2.3rem',md:'2.5rem!important'}}}
-                    />
-                </Toolbar>
-            </AppBar >
-        </motion.div>
-    )
-}
-
-export default PlaceSearchingNavigationbar
+export default PlaceSearchingNavigationbar;
--- src/components/SearchBar/ReduceNavigation.js
+++ src/components/SearchBar/ReduceNavigation.js
@@ -1,55 +1,100 @@
 import { AppBar, Toolbar, Button } from "@mui/material";
-import { Bolt as BoltIcon, ArrowCircleLeftOutlined as ArrowCircleLeftOutlinedIcon } from "@mui/icons-material";
+import {
+  Bolt as BoltIcon,
+  ArrowCircleLeftOutlined as ArrowCircleLeftOutlinedIcon,
+} from "@mui/icons-material";
 import { useStyles } from "./style";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
+import "./style";
+const ReduceNavigation = ({
+  handleOnclickSource,
+  searchCoordinates,
+  handleOnclickDestination,
+  handleOnclickSearchLocation,
+}) => {
+  // Styles
+  const classes = useStyles();
 
-const ReduceNavigation = ({ handleOnclickSource, searchCoordinates, handleOnclickDestination,
-    handleOnclickSearchLocation }) => {
+  return (
+    <motion.div>
+      <AppBar
+        className={classes.rdNavigation}
+        sx={{
+          borderRadius: { xs: "0rem", sm: "10rem" },
+          padding: { xs: "0.6rem", sm: "0rem" },
+          width: { xs: "100vw", sm: "70vw", md: "46vw" },
+          left: {
+            xs: "0rem",
+            sm: "calc(50% - 33vw)!important",
+            md: "calc(50% - 23vw)!important",
+          },
+          top: { xs: "0rem", sm: "2.5rem" },
+        }}
+      >
+        <Toolbar
+          className={classes.newtoolbarstyle}
+          sx={{
+            flexDirection: { xs: "column", sm: "row" },
+            padding: { xs: "0.3rem", sm: "0.1rem" },
+          }}
+        >
+          <Button
+            id="source-button"
+            aria-describedby="simple-popover"
+            onClick={handleOnclickSource}
+            className={classes.inputBtns}
+            sx={{
+              width: { xs: "90%", sm: "100%" },
+              borderRadius: { xs: "10px", sm: "10rem" },
+            }}
+          >
+            {searchCoordinates && searchCoordinates.source.label !== ""
+              ? searchCoordinates.source.label.substring(0, 26)
+              : "Source"}
+            {searchCoordinates && searchCoordinates.source.label.length > 26
+              ? "..."
+              : ""}
+          </Button>
+          <BoltIcon
+            className={classes.boltIcon}
+            sx={{
+              margin: { xs: "0.5rem 1rem", sm: "0 1rem" },
+              fontSize: { xs: "1.5rem", sm: "2.5rem" },
+            }}
+          />
+          <Button
+            id="destination-button"
+            maxLength={10}
+            className={classes.inputBtns}
+            aria-describedby="simple-popover"
+            onClick={handleOnclickDestination}
+            sx={{
+              width: { xs: "90%", sm: "100%" },
+              borderRadius: { xs: "10px", sm: "10rem" },
+            }}
+          >
+            {searchCoordinates && searchCoordinates.destination.label !== ""
+              ? searchCoordinates.destination.label.substring(0, 26)
+              : "Destination"}
+            {searchCoordinates &&
+            searchCoordinates.destination.label.length > 26
+              ? "..."
+              : ""}
+          </Button>
+          <ArrowCircleLeftOutlinedIcon
+            className={classes.arrowBackIcon}
+            onClick={handleOnclickSearchLocation}
+            sx={{
+              right: { xs: "0rem", sm: "-5rem" },
+              top: { xs: "3.3rem", sm: "auto" },
+              border: { xs: "2px solid black", sm: "3px solid black " },
+              fontSize: { xs: "2rem", sm: "3rem" },
+            }}
+          />
+        </Toolbar>
+      </AppBar>
+    </motion.div>
+  );
+};
 
-    // Styles
-    const classes = useStyles();
-
-    return (
-        <motion.div>
-            <AppBar className={classes.rdNavigation}
-          
-            
-            sx={{ borderRadius: {xs: '0rem', sm: '10rem'},padding:{xs:"0.6rem",sm:"0rem"},width:{xs:"100vw",sm:"70vw",md:"46vw"},left:{xs:"0rem",sm:"calc(50% - 33vw)!important",md:"calc(50% - 23vw)!important"},top:{xs:"0rem",sm:"2.5rem"}}}
-            
-            >
-                <Toolbar className={classes.newtoolbarstyle}
-                sx={{ flexDirection: {xs: 'column', sm: 'row'},padding:{xs:"0.3rem",sm:"0.1rem"}}}
-                >
-                    <Button id="source-button" aria-describedby='simple-popover'
-                        onClick={handleOnclickSource} className={classes.inputBtns}
-                        sx={{ width: {xs: '90%', sm: '100%'},borderRadius:{xs:"10px",sm:"10rem"}}}
-                    >
-                        {searchCoordinates && searchCoordinates.source.label !== ''
-                            ? searchCoordinates.source.label.substring(0, 26)
-                            : 'Source'}
-                        {searchCoordinates && searchCoordinates.source.label.length > 26 ? '...' : ''}
-                    </Button>
-                    <BoltIcon className={classes.boltIcon} 
-                    sx={{ margin:{xs:"0.5rem 1rem",sm:"0 1rem"},fontSize:{xs:"1.5rem",sm:"2.5rem"}}}
-                    />
-                    <Button id="destination-button" maxLength={10} className={classes.inputBtns}
-                        aria-describedby='simple-popover' onClick={handleOnclickDestination} 
-                        sx={{ width: {xs: '90%', sm: '100%'},borderRadius:{xs:"10px",sm:"10rem"}}}                       
-                    >
-                        {searchCoordinates && searchCoordinates.destination.label !== ''
-                            ? searchCoordinates.destination.label.substring(0, 26)
-                            : 'Destination'}
-                        {searchCoordinates && searchCoordinates.destination.label.length > 26 ? '...' : ''}
-                    </Button>
-                    <ArrowCircleLeftOutlinedIcon
-                        className={classes.arrowBackIcon}
-                        onClick={handleOnclickSearchLocation}
-                        sx={{ right: {xs: '0rem', sm: '-5rem'},top:{xs:"3.3rem",sm:"auto"},border:{xs:"2px solid black",sm:"3px solid black "},fontSize:{xs:"2rem",sm:"3rem"}}}
-                    />
-                </Toolbar>
-            </AppBar >
-        </motion.div>
-    )
-}
-
-export default ReduceNavigation
+export default ReduceNavigation;
